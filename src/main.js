e87b72412cbec47fd65fb0114ead09210572e703
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-02-28 14:31:35 +0900

    testing @vueuse/motion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
+
+Vue.use(VueCompositionAPI)
+Vue.use(MotionPlugin)
 
 Vue.config.productionTip = false
 
